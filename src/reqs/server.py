7e7be4ef4c639de566baf34477d591bb8f6dbd55
nana brown <nanabrownsnr@gmail.com>
2026-08-29 23:49:22 +0100

    fixed import

--- src/reqs/server.py
+++ src/reqs/server.py
@@ -4,7 +4,7 @@ from starlette.requests import Request
 from starlette.responses import JSONResponse
 
 from reqs.telegram_service import handle_telegram_update, register_telegram_webhook
-from reqs.services import save_tenant
+from reqs.services import _save_tenant
 
 
 def build_server(graph):
